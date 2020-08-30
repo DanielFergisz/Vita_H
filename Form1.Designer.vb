f8681884cb89636b229c4978981d68eb2a7b6361
Daniel Fergisz <daniel.fergisz@gmail.com>
2020-08-30 22:50:14 +0200

    1.01 - add erase and validate directories, add errors info

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -38,79 +38,92 @@ Partial Class Form1
         'Log1
         '
         Me.Log1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-        Me.Log1.Location = New System.Drawing.Point(12, 12)
+        Me.Log1.Location = New System.Drawing.Point(16, 15)
+        Me.Log1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Log1.Name = "Log1"
         Me.Log1.ReadOnly = True
         Me.Log1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
-        Me.Log1.Size = New System.Drawing.Size(315, 325)
+        Me.Log1.Size = New System.Drawing.Size(419, 399)
         Me.Log1.TabIndex = 0
         Me.Log1.Text = ""
         '
         'Check_Dir
         '
-        Me.Check_Dir.Enabled = False
-        Me.Check_Dir.Location = New System.Drawing.Point(333, 12)
+        Me.Check_Dir.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Check_Dir.Location = New System.Drawing.Point(444, 15)
+        Me.Check_Dir.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Check_Dir.Name = "Check_Dir"
-        Me.Check_Dir.Size = New System.Drawing.Size(133, 23)
+        Me.Check_Dir.Size = New System.Drawing.Size(177, 28)
         Me.Check_Dir.TabIndex = 1
         Me.Check_Dir.Text = "Check"
         Me.Check_Dir.UseVisualStyleBackColor = True
         '
         'S1
         '
-        Me.S1.Location = New System.Drawing.Point(333, 54)
+        Me.S1.Enabled = False
+        Me.S1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.S1.Location = New System.Drawing.Point(444, 66)
+        Me.S1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.S1.Name = "S1"
-        Me.S1.Size = New System.Drawing.Size(133, 35)
+        Me.S1.Size = New System.Drawing.Size(177, 43)
         Me.S1.TabIndex = 2
         Me.S1.Text = "Step 1"
         Me.S1.UseVisualStyleBackColor = True
         '
         'S2
         '
-        Me.S2.Location = New System.Drawing.Point(333, 95)
+        Me.S2.Enabled = False
+        Me.S2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.S2.Location = New System.Drawing.Point(444, 117)
+        Me.S2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.S2.Name = "S2"
-        Me.S2.Size = New System.Drawing.Size(133, 33)
+        Me.S2.Size = New System.Drawing.Size(177, 41)
         Me.S2.TabIndex = 3
         Me.S2.Text = "Step 2"
         Me.S2.UseVisualStyleBackColor = True
         '
         'dirName
         '
-        Me.dirName.Location = New System.Drawing.Point(12, 343)
+        Me.dirName.Location = New System.Drawing.Point(16, 422)
+        Me.dirName.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.dirName.Name = "dirName"
-        Me.dirName.Size = New System.Drawing.Size(111, 20)
+        Me.dirName.Size = New System.Drawing.Size(147, 22)
         Me.dirName.TabIndex = 4
         Me.dirName.Visible = False
         '
         'Key
         '
-        Me.Key.Location = New System.Drawing.Point(12, 369)
+        Me.Key.Location = New System.Drawing.Point(16, 454)
+        Me.Key.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Key.Name = "Key"
-        Me.Key.Size = New System.Drawing.Size(454, 20)
+        Me.Key.Size = New System.Drawing.Size(604, 22)
         Me.Key.TabIndex = 5
         Me.Key.Visible = False
         '
         'UN
         '
-        Me.UN.Location = New System.Drawing.Point(129, 343)
+        Me.UN.Location = New System.Drawing.Point(172, 422)
+        Me.UN.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.UN.Name = "UN"
-        Me.UN.Size = New System.Drawing.Size(100, 20)
+        Me.UN.Size = New System.Drawing.Size(132, 22)
         Me.UN.TabIndex = 6
         Me.UN.Visible = False
         '
         'Len1
         '
-        Me.Len1.Location = New System.Drawing.Point(235, 343)
+        Me.Len1.Location = New System.Drawing.Point(313, 422)
+        Me.Len1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Len1.Name = "Len1"
-        Me.Len1.Size = New System.Drawing.Size(36, 20)
+        Me.Len1.Size = New System.Drawing.Size(47, 22)
         Me.Len1.TabIndex = 7
         Me.Len1.Visible = False
         '
         'Str1
         '
-        Me.Str1.Location = New System.Drawing.Point(334, 160)
+        Me.Str1.Location = New System.Drawing.Point(445, 197)
+        Me.Str1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Str1.Name = "Str1"
-        Me.Str1.Size = New System.Drawing.Size(132, 177)
+        Me.Str1.Size = New System.Drawing.Size(175, 217)
         Me.Str1.TabIndex = 8
         Me.Str1.Text = ""
         Me.Str1.Visible = False
@@ -119,26 +132,28 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Alef", 24.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(181, 344)
+        Me.Label1.Location = New System.Drawing.Point(241, 423)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(99, 45)
+        Me.Label1.Size = New System.Drawing.Size(127, 57)
         Me.Label1.TabIndex = 9
         Me.Label1.Text = "VITA"
         '
         'TB1
         '
-        Me.TB1.Location = New System.Drawing.Point(334, 134)
+        Me.TB1.Location = New System.Drawing.Point(445, 165)
+        Me.TB1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TB1.Name = "TB1"
-        Me.TB1.Size = New System.Drawing.Size(132, 20)
+        Me.TB1.Size = New System.Drawing.Size(175, 22)
         Me.TB1.TabIndex = 10
         Me.TB1.Visible = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(478, 395)
+        Me.ClientSize = New System.Drawing.Size(637, 486)
         Me.Controls.Add(Me.TB1)
         Me.Controls.Add(Me.Key)
         Me.Controls.Add(Me.UN)
@@ -150,9 +165,10 @@ Partial Class Form1
         Me.Controls.Add(Me.Check_Dir)
         Me.Controls.Add(Me.Log1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Vita_H 3.73 v1.00"
+        Me.Text = "Vita_H 3.73 v1.01"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
--- Form1.vb
+++ Form1.vb
@@ -14,12 +14,21 @@ Public Class Form1
         Log1.AppendText(Environment.NewLine + "Checking system...")
         If Directory.Exists("C:\Users\" & UN.Text & "\Documents\PS Vita") Then
             Log1.AppendText(Environment.NewLine + "Preparing...")
-            Directory.Delete("C:\Users\" & UN.Text & "\Documents\PS Vita")
+            Log1.AppendText(Environment.NewLine + "Deleting old files...")
+            Directory.Delete("C:\Users\" & UN.Text & "\Documents\PS Vita", True)
+            Log1.SelectionColor = Color.ForestGreen
+            Log1.AppendText("OK")
+            Log1.SelectionColor = Color.Empty
             Log1.AppendText(Environment.NewLine + "Done.")
-            Log1.AppendText(Environment.NewLine + "Please Run QCMA")
-
+            Log1.SelectionColor = Color.DarkRed
+            Log1.AppendText(Environment.NewLine + "Please connect PS VITA and Run QCMA")
+            Log1.SelectionColor = Color.Empty
+        Else
+            Log1.SelectionColor = Color.DarkRed
+            Log1.AppendText(Environment.NewLine + "Please connect PS VITA and Run QCMA")
+            Log1.SelectionColor = Color.Empty
         End If
-
+        S1.Enabled = True
     End Sub
 
     Private Sub S1_Click(sender As Object, e As EventArgs) Handles S1.Click
@@ -27,46 +36,53 @@ Public Class Form1
         Log1.AppendText(Me.Text)
         Log1.AppendText(Environment.NewLine + "")
         Log1.AppendText(Environment.NewLine + "Geting info..")
-        For Each Dir As String In Directory.GetDirectories("C:\Users\" & UN.Text & "\Documents\PS Vita\APP") 'pobiera nazwy folderów
-            dirName.Text = Mid(Dir, Len1.Text + 1, 20) 'wycina część tekstu ze stringa i wkleja w textbox
-        Next
-        Log1.SelectionColor = Color.ForestGreen
-        Log1.AppendText("OK")
-        Log1.SelectionColor = Color.Empty
+        If Directory.Exists("C:\Users\" & UN.Text & "\Documents\PS Vita\APP") Then
+            For Each Dir As String In Directory.GetDirectories("C:\Users\" & UN.Text & "\Documents\PS Vita\APP") 'pobiera nazwy folderów
+                dirName.Text = Mid(Dir, Len1.Text + 1, 20) 'wycina część tekstu ze stringa i wkleja w textbox
+            Next
+            Log1.SelectionColor = Color.ForestGreen
+            Log1.AppendText("OK")
+            Log1.SelectionColor = Color.Empty
 
-        Log1.AppendText(Environment.NewLine + "Calculate key...")
-        Dim webClient As New System.Net.WebClient
-        Dim result As String = webClient.DownloadString("http://cma.henkaku.xyz/?aid=" + dirName.Text) 'przechodzi na strone i pobiera treść do stringa
-        Str1.AppendText(Environment.NewLine + result) 'wkleja zawartość strony do pola tekstowego
-        Key.Text = Mid(Str1.Text, 262, 65) 'pobiera klucz z pola tekstowego
-        Log1.SelectionColor = Color.ForestGreen
-        Log1.AppendText("OK")
-        Log1.SelectionColor = Color.Empty
-        Log1.AppendText(Environment.NewLine + "Key: ")
-        Log1.SelectionColor = Color.DarkRed
-        Log1.AppendText(Key.Text)
-        Log1.SelectionColor = Color.Empty
+            Log1.AppendText(Environment.NewLine + "Calculate key...")
+            Dim webClient As New System.Net.WebClient
+            Dim result As String = webClient.DownloadString("http://cma.henkaku.xyz/?aid=" + dirName.Text) 'przechodzi na strone i pobiera treść do stringa
+            Str1.AppendText(Environment.NewLine + result) 'wkleja zawartość strony do pola tekstowego
+            Key.Text = Mid(Str1.Text, 262, 65) 'pobiera klucz z pola tekstowego
+            Log1.SelectionColor = Color.ForestGreen
+            Log1.AppendText("OK")
+            Log1.SelectionColor = Color.Empty
+            Log1.AppendText(Environment.NewLine + "Key: ")
+            Log1.SelectionColor = Color.DarkRed
+            Log1.AppendText(Key.Text)
+            Log1.SelectionColor = Color.Empty
 
-        Log1.AppendText(Environment.NewLine + "")
-        Log1.AppendText(Environment.NewLine + "Preparing files, please wait..")
-        If Key.Text.Length = 0 Then
-            Log1.AppendText(Environment.NewLine + "Error 0x33940") 'Brak wygenerowanego klucza
-        Else
-            Threading.Thread.Sleep(1000)
-            If File.Exists("psvimg-create.exe") = True Then
-                Process.Start("cmd", "/c psvimg-create -n app -K " + Key.Text + " app PCSG90096/app").WaitForExit()
-                Log1.AppendText(Environment.NewLine + "App.. ok")
-                Process.Start("cmd", "/c psvimg-create -n appmeta -K " + Key.Text + " appmeta PCSG90096/appmeta").WaitForExit()
-                Log1.AppendText(Environment.NewLine + "AppMeta.. ok")
-                Process.Start("cmd", "/c psvimg-create -n license -K " + Key.Text + " license PCSG90096/license").WaitForExit()
-                Log1.AppendText(Environment.NewLine + "License.. ok")
-                Process.Start("cmd", "/c psvimg-create -n savedata -K " + Key.Text + " savedata PCSG90096/savedata").WaitForExit()
-                Log1.AppendText(Environment.NewLine + "SaveData.. ok")
-                Log1.AppendText(Environment.NewLine + "Done.")
+            Log1.AppendText(Environment.NewLine + "")
+            Log1.AppendText(Environment.NewLine + "Preparing files, please wait..")
+            If Key.Text.Length = 0 Then
+                Log1.AppendText(Environment.NewLine + "Error 0x33940") 'Brak wygenerowanego klucza
             Else
-                Log1.AppendText(Environment.NewLine + "Error 0x45950") 'Brak pliku exe w folderze z aplikacją
+                Threading.Thread.Sleep(1000)
+                If File.Exists("psvimg-create.exe") = True Then
+                    Process.Start("cmd", "/c psvimg-create -n app -K " + Key.Text + " app PCSG90096/app").WaitForExit()
+                    Log1.AppendText(Environment.NewLine + "App.. ok")
+                    Process.Start("cmd", "/c psvimg-create -n appmeta -K " + Key.Text + " appmeta PCSG90096/appmeta").WaitForExit()
+                    Log1.AppendText(Environment.NewLine + "AppMeta.. ok")
+                    Process.Start("cmd", "/c psvimg-create -n license -K " + Key.Text + " license PCSG90096/license").WaitForExit()
+                    Log1.AppendText(Environment.NewLine + "License.. ok")
+                    Process.Start("cmd", "/c psvimg-create -n savedata -K " + Key.Text + " savedata PCSG90096/savedata").WaitForExit()
+                    Log1.AppendText(Environment.NewLine + "SaveData.. ok")
+                    Log1.AppendText(Environment.NewLine + "Done.")
+                Else
+                    Log1.AppendText(Environment.NewLine + "Error 0x45950") 'Brak pliku exe w folderze z aplikacją
+                End If
             End If
+            S2.Enabled = True
         End If
+        Log1.SelectionColor = Color.Red
+        Log1.AppendText(" False !!")
+        Log1.SelectionColor = Color.Empty
+        MsgBox("Please connect PS VITA and Run QCMA first !!")
     End Sub
 
     Private Sub S2_Click(sender As Object, e As EventArgs) Handles S2.Click
@@ -82,12 +98,13 @@ Public Class Form1
     End Sub
     '#############################################################################################################################################
     Private Sub Log1_DoubleClick(sender As Object, e As EventArgs) Handles Log1.DoubleClick
-        Check_Dir.Enabled = True
         TB1.Visible = True
+        S1.Enabled = True
+        S2.Enabled = True
     End Sub
 
     Private Sub TB1_TextChanged(sender As Object, e As EventArgs) Handles TB1.TextChanged
-        If TB1.Text = "Dragon" Then
+        If TB1.Text = "$admin" Then
             dirName.Visible = True
             UN.Visible = True
             Len1.Visible = True
@@ -95,7 +112,10 @@ Public Class Form1
             Str1.Visible = True
             Log1.AppendText(Environment.NewLine + "God Mode Enabled")
         End If
-        If TB1.Text = "User" Then
+        If TB1.Text = "$info" Then
+            MsgBox("Error 0x64452 - Brak spathowanych plików" + vbCrLf + "Error 0x45950 - Brak pliku exe w folderze z aplikacją" + vbCrLf + "Error 0x33940 - Brak wygenerowanego klucza")
+        End If
+        If TB1.Text = "$user" Then
             dirName.Visible = False
             UN.Visible = False
             Len1.Visible = False
@@ -105,5 +125,4 @@ Public Class Form1
             Log1.AppendText(Me.Text)
         End If
     End Sub
-
 End Class
